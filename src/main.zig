6e1c88b48a3aa0b4bfcd63c00c962f489c109fc7
wipeseals <wipeseals@gmail.com>
2025-04-11 00:32:38 +0900

    feat(wip): define Vec3

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,51 @@
 const std = @import("std");
+const allocator = std.heap.page_allocator;
+
+const Vec3 = struct {
+    x: f32,
+    y: f32,
+    z: f32,
+
+    fn new(x: f32, y: f32, z: f32) Vec3 {
+        return Vec3{ .x = x, .y = y, .z = z };
+    }
+    fn add(self: Vec3, other: *Vec3) Vec3 {
+        return Vec3{ .x = self.x + other.x, .y = self.y + other.y, .z = self.z + other.z };
+    }
+    fn sub(self: Vec3, other: *Vec3) Vec3 {
+        return Vec3{ .x = self.x - other.x, .y = self.y - other.y, .z = self.z - other.z };
+    }
+    fn mul(self: Vec3, t: f32) Vec3 {
+        return Vec3{ .x = self.x * t, .y = self.y * t, .z = self.z * t };
+    }
+    fn div(self: Vec3, t: f32) Vec3 {
+        return Vec3{ .x = self.x / t, .y = self.y / t, .z = self.z / t };
+    }
+    fn length(self: Vec3) f32 {
+        return @sqrt(self.x * self.x + self.y * self.y + self.z * self.z);
+    }
+    fn length_squared(self: Vec3) f32 {
+        return self.x * self.x + self.y * self.y + self.z * self.z;
+    }
+    fn dot(self: Vec3, other: *Vec3) f32 {
+        return self.x * other.x + self.y * other.y + self.z * other.z;
+    }
+    fn cross(self: Vec3, other: *Vec3) Vec3 {
+        return Vec3{
+            .x = self.y * other.z - self.z * other.y,
+            .y = self.z * other.x - self.x * other.z,
+            .z = self.x * other.y - self.y * other.x,
+        };
+    }
+    fn to_string(self: Vec3) []const u8 {
+        return std.fmt.allocPrint(allocator, "{d} {d} {d}", .{
+            @as(u8, (255.999 * self.x) & 0xff),
+            @as(u8, (255.999 * self.y) & 0xff),
+            @as(u8, (255.999 * self.z) & 0xff),
+        });
+    }
+};
+const Color = Vec3;
 
 pub fn main() !void {
     const filename = "output.ppm";
@@ -11,14 +58,15 @@ pub fn main() !void {
     // print ppm format
     try file_writer.print("P3\n{d} {d}\n255\n", .{ width, height });
     for (0..width) |x| {
-        for (0..height) |y| {
-            // calculate the color
-            const r = x * 255 / width;
-            const g = y * 255 / height;
-            const b = ((x + y) * 255) / (width + height);
+        for (0..height) |_| {
+            const color = Color.new(
+                0.1,
+                0.2,
+                0.25,
+            );
 
             // print the color
-            try file_writer.print("{d} {d} {d}\n", .{ r, g, b });
+            try file_writer.write(color.to_string());
         }
         std.debug.print("progress: {d} % ({d}/{d})\n", .{
             (x * 100) / width,
