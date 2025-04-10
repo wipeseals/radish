8f7ddfa9f7c71a34b41bff5861ef16ba1aa6b19c
wipeseals <wipeseals@gmail.com>
2025-04-11 00:02:56 +0900

    feat: implement PPM file generation in main function

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,24 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("Hello, Zig!\n", .{});
+pub fn main() !void {
+    const filename = "output.ppm";
+    const file = try std.fs.cwd().createFile(filename, .{});
+    const file_writer = file.writer();
+    defer file.close();
+    const width: u32 = 256;
+    const height: u32 = 256;
+
+    // print ppm format
+    try file_writer.print("P3\n{d} {d}\n255\n", .{ width, height });
+    for (0..width) |x| {
+        for (0..height) |y| {
+            // calculate the color
+            const r = x * 255 / width;
+            const g = y * 255 / height;
+            const b = ((x + y) * 255) / (width + height);
+
+            // print the color
+            try file_writer.print("{d} {d} {d}\n", .{ r, g, b });
+        }
+    }
 }
