e238f899c3ca6217d20e78e84bcebf0046c80f04
wipeseals <wipeseals@gmail.com>
2025-04-15 00:48:25 +0900

    feat: レイの射出と背景描画を実装

--- src/main.zig
+++ src/main.zig
@@ -9,10 +9,10 @@ const Vec3 = struct {
     fn new(x: f32, y: f32, z: f32) Vec3 {
         return Vec3{ .x = x, .y = y, .z = z };
     }
-    fn add(self: Vec3, other: *Vec3) Vec3 {
+    fn add(self: Vec3, other: *const Vec3) Vec3 {
         return Vec3{ .x = self.x + other.x, .y = self.y + other.y, .z = self.z + other.z };
     }
-    fn sub(self: Vec3, other: *Vec3) Vec3 {
+    fn sub(self: Vec3, other: *const Vec3) Vec3 {
         return Vec3{ .x = self.x - other.x, .y = self.y - other.y, .z = self.z - other.z };
     }
     fn mul(self: Vec3, t: f32) Vec3 {
@@ -27,10 +27,10 @@ const Vec3 = struct {
     fn length_squared(self: Vec3) f32 {
         return self.x * self.x + self.y * self.y + self.z * self.z;
     }
-    fn dot(self: Vec3, other: *Vec3) f32 {
+    fn dot(self: Vec3, other: *const Vec3) f32 {
         return self.x * other.x + self.y * other.y + self.z * other.z;
     }
-    fn cross(self: Vec3, other: *Vec3) Vec3 {
+    fn cross(self: Vec3, other: *const Vec3) Vec3 {
         return Vec3{
             .x = self.y * other.z - self.z * other.y,
             .y = self.z * other.x - self.x * other.z,
@@ -47,31 +47,61 @@ const Vec3 = struct {
 };
 const Color = Vec3;
 
+const Ray = struct {
+    origin: Vec3,
+    direction: Vec3,
+
+    fn new(origin: Vec3, direction: Vec3) Ray {
+        return Ray{ .origin = origin, .direction = direction };
+    }
+    fn at(self: Ray, t: f32) Vec3 {
+        return self.origin.add(&self.direction.mul(t));
+    }
+};
+
+pub fn bg_color(ray: Ray) Color {
+    const unit_direction = ray.direction.div(ray.direction.length());
+    const t = 0.5 * (unit_direction.y + 1.0);
+    return Color.new(1.0, 1.0, 1.0).mul(1.0 - t).add(&Color.new(0.5, 0.7, 1.0).mul(t));
+}
+
 pub fn main() !void {
     const filename = "output.ppm";
     const file = try std.fs.cwd().createFile(filename, .{});
     const file_writer = file.writer();
     defer file.close();
-    const width: u32 = 256;
-    const height: u32 = 256;
+
+    const aspect_ratio: f32 = 16.0 / 9.0;
+    const image_width: u32 = 384;
+    const image_height: u32 = @as(u32, @intFromFloat(@as(f32, image_width) / @as(f32, aspect_ratio)));
+
+    const viewport_height = 2.0;
+    const viewport_width: f32 = aspect_ratio * viewport_height;
+    const origin = Vec3.new(0.0, 0.0, 0.0);
+    const horizontal = Vec3.new(viewport_width, 0.0, 0.0);
+    const vertical = Vec3.new(0.0, viewport_height, 0.0);
+    const lower_left_corner = origin.sub(&horizontal.div(2)).sub(&vertical.div(2)).sub(&Vec3.new(0.0, 1.0, 1.0));
 
     // print ppm format
-    try file_writer.print("P3\n{d} {d}\n255\n", .{ width, height });
-    for (0..width) |x| {
-        for (0..height) |y| {
-            const color = Color.new(
-                @as(f32, @floatFromInt(x)) / @as(f32, @floatFromInt(width - 1)),
-                @as(f32, @floatFromInt(y)) / @as(f32, @floatFromInt(height - 1)),
-                0.25,
+    try file_writer.print("P3\n{d} {d}\n255\n", .{ image_width, image_height });
+    for (0..image_height) |j| { // bottom to top
+        const y = image_height - j - 1;
+        for (0..image_width) |i| { // left to right
+            const x = i;
+            // calculate the ray for the pixel
+            const u = @as(f32, @floatFromInt(x)) / @as(f32, @floatFromInt(image_width - 1));
+            const v = @as(f32, @floatFromInt(y)) / @as(f32, @floatFromInt(image_height - 1));
+            const ray = Ray.new(
+                origin,
+                lower_left_corner.add(&horizontal.mul(u)).add(&vertical.mul(v)).sub(&origin),
             );
-
+            const color = bg_color(ray);
             // print the color
             _ = try file_writer.write(try color.to_string());
         }
-        std.debug.print("progress: {d} % ({d}/{d})\n", .{
-            (x * 100) / width,
-            x,
-            width,
+        std.debug.print("progress: {d}/{d}\n", .{
+            j,
+            image_height,
         });
     }
 }
